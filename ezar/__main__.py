69549c5a146c5748077cdf6407389dc1f02500ea
Noelle <noelle_km@proton.me>
2022-06-15 15:38:59 +0200

    fix(backend): change `logging` date format

--- ezar/__main__.py
+++ ezar/__main__.py
@@ -8,7 +8,7 @@ from logging import INFO, getLogger, basicConfig
 bot = Ezar()
 log = getLogger(__name__)
 basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s: %(message)s",
+    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
     level=INFO,
     datefmt="%Y-%m-%d - %H:%M:%S",
 )
