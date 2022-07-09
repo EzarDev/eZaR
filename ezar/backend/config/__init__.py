cf3dfd120b1faa4d63a59e4146dff39bc24a4c3a
Sham <thesham@proton.me>
2022-07-09 19:27:58 +0200

    refactor: change `config.constants` to `config`

--- ezar/backend/config/__init__.py
+++ ezar/backend/config/__init__.py
@@ -1,8 +1,64 @@
-from .constants import Bot, Colors, Config, Emojis
+from os import environ
 
-__all__ = (
-    "Bot",
-    "Colors",
-    "Config",
-    "Emojis",
+from disnake import Permissions
+from motor.core import (
+    Collection as MotorCollection,
+    Database as MotorDatabase
 )
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+__all__ = ("Bot", "Config", "Colors", "Emojis", "Database",)
+
+
+class Bot:
+    name = "eZaR"
+    version = "3.0.0a"
+    gh_repo = "https://github.com/eZaR-Bot/3Z4R"
+    pp_gist = ""
+    ppal_link = "https://paypal.me/realShamlol"
+    pat_link = "https://patreon.com/ezarbot"
+    invite_perms = Permissions(
+        attach_files=True,
+        ban_members=True,
+        embed_links=True,
+        external_emojis=True,
+        kick_members=True,
+        manage_channels=True,
+        manage_guild=True,
+        manage_messages=True,
+        manage_roles=True,
+    )
+    support_inv_url = "2Usu8E5KK4"
+
+
+class Config:
+    main_token = environ.get("MAIN_TOKEN")
+    beta_token = environ.get("BETA_TOKEN")
+    mongo_uri = environ.get("MONGO_URI")
+    owner_ids = [int(id_) for id_ in environ.get("OWNER_IDS").split(", ")]
+    test_guilds = int(environ["TEST_GUILDS"])
+    blist_log_channel_id = int(environ.get("LIST_POST_CHANNEL_ID"))
+    bot_logs_channel_id = int(environ.get("BOT_LOGS_CHANNEL_ID"))
+
+
+class Colors:
+    purple = 0x8449CA
+    green = 0x27A027
+    white = 0xFFFFFF
+    grey = 0x5B5959
+    red = 0xFE4D4D
+    yellow = 0xFEFE4D
+
+
+class Emojis:
+    cross = "<:eZaR_cross:911561593811271741>"
+    tick = "<:eZaR_tick:911561509677727764>"
+
+
+class Database:
+    _cluster = AsyncIOMotorClient(Config.mongo_uri)
+    _db: MotorDatabase = _cluster["eZaR"]
+    guild_logs: MotorCollection = _db["guild_logs"]
