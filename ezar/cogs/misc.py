57fd42e38836ea263b20428cb8faba61451ba9fe
Sham <thesham@proton.me>
2022-07-17 22:39:15 +0200

    feat: add a guild info command
    style(info-server): add Created, Rules channel, & vanity URL fields
    
    refactor(info-guild): sort roles in order of position
    
    refactor(info-guild): apply suggested changes

--- ezar/cogs/misc.py
+++ ezar/cogs/misc.py
@@ -1,6 +1,7 @@
+from inspect import cleandoc
 from typing import Literal, Optional
 
-from disnake import CommandInter, Member, User
+from disnake import CommandInter, Member, User, utils
 from disnake.ext.commands import Cog, slash_command
 
 from ezar import Ezar
@@ -8,7 +9,7 @@ from ezar.backend.config import Colors
 from ezar.utils.embed import Embeb
 
 
-class Miscellaneous(Cog):
+class Miscellaneous(Cog, slash_command_attrs={"dm_permissions": False}):
     """Miscellaneous/not-important commands"""
 
     def __init__(self, bot: Ezar):
@@ -22,10 +23,13 @@ class Miscellaneous(Cog):
     async def icon_server(self, itr: CommandInter):
         """Returns the server icon URL."""
         avatar_embed = Embeb()
-        avatar_embed.set_image(url=itr.guild.icon.with_size(1024).url)
+        avatar_embed.set_image(
+            url=itr.guild.icon.with_size(1024).url if itr.guild.icon else None
+        )
+        # TODO: generate an image instead of returning None
         avatar_embed.set_author(
             name=itr.guild.name,
-            icon_url=itr.guild.icon if itr.guild.icon else itr.guild.gen,
+            icon_url=itr.guild.icon if itr.guild.icon else None,
         )
         await itr.response.send_message(embed=avatar_embed)
 
@@ -66,12 +70,63 @@ class Miscellaneous(Cog):
     @info.sub_command("server")
     async def info_server(self, itr: CommandInter):
         """Returns the server information."""
-        info_embed = Embeb(
-            description=itr.guild.description,
+        guild = itr.guild
+        members = cleandoc(
+            f"""
+        Humans: `{len([m for m in guild.members if not m.bot])}`
+        Bots: `{len([m for m in guild.members if m.bot])}`
+        """
         )
-        info_embed.set_author(name=itr.guild.name, icon_url=itr.guild.icon)
-        info_embed.set_thumbnail(url=itr.guild.icon.url if itr.guild.icon else "")
-        info_embed.add_field(name="Owner", value=f"`{itr.guild.owner}`")
+        channels = cleandoc(
+            f"""
+        Text: `{len(guild.text_channels)}`
+        Voice: `{len(guild.voice_channels)}`
+        Stages: `{len(guild.stage_channels)}`
+        Forums: `{len(guild.forum_channels)}`
+        """
+        )
+        if len(guild.roles) > 25:
+            roles = "Too many roles to display."
+        else:
+            roles = ", ".join(
+                sorted(
+                    [r.mention for r in guild.roles if r.id != guild.id],
+                    key=lambda r: r.position,
+                )
+            )
+        vanity = await guild.vanity_invite(use_cached=True)
+        info_embed = Embeb(
+            description=guild.description,
+        )
+        info_embed.set_author(name=guild.name, icon_url=guild.icon)
+        info_embed.set_thumbnail(url=guild.icon.url if guild.icon else "")
+        info_embed.set_image(
+            url=guild.banner.url
+            if guild.banner
+            else guild.splash.url
+            if guild.splash
+            else ""
+        )
+        info_embed.add_field(name="Owner", value=guild.owner)
+        info_embed.add_field(name="ID", value=guild.id)
+        info_embed.add_field(name=f"Members [{len(guild.members)}]", value=members)
+        info_embed.add_field(name="Created", value=utils.format_dt(guild.created_at)),
+        info_embed.add_field(
+            name="Rule Channel",
+            value=guild.rules_channel.mention
+            if guild.rules.channel
+            else "No rules channel",
+        )
+        info_embed.add_field(
+            name="Vanity URL",
+            value=f"[{guild.vanity_url_code}]({vanity.url if vanity else ''})",
+        )
+        info_embed.add_field(
+            name=f"Channels [{len(guild.channels)}]", value=channels, inline=False
+        )
+        info_embed.add_field(
+            name=f"Roles [{len(guild.roles)}]", value=roles, inline=False
+        )
         await itr.response.send_message(embed=info_embed)
 
 
