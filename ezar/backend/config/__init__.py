9c19c74b78fe9c4e7bd69f860b315d54ac27fcd3
Sham <thesham@proton.me>
2022-07-31 18:14:36 +0000

    refactor(env): allow list on owner IDs

--- ezar/backend/config/__init__.py
+++ ezar/backend/config/__init__.py
@@ -36,7 +36,7 @@ class Config:
     main_token = environ.get("MAIN_TOKEN")
     beta_token = environ.get("BETA_TOKEN")
     mongo_uri = environ.get("MONGO_URI")
-    owner_ids = [int(id_) for id_ in environ["OWNER_IDS"].split(", ")]
+    owner_ids = [int(id_) for id_ in environ["OWNER_IDS"].strip("[]").split(", ")]
     test_guilds = int(environ["TEST_GUILDS"])
     blist_log_channel_id = int(environ["LIST_POST_CHANNEL_ID"])
     bot_logs_channel_id = int(environ["BOT_LOGS_CHANNEL_ID"])
