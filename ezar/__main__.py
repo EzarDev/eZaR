33fb0546d02bbcf10b00fd852c43a01234786acf
Sham <thesham@proton.me>
2022-07-31 14:08:14 +0200

    style(logging): wrap the time

--- ezar/__main__.py
+++ ezar/__main__.py
@@ -13,7 +13,7 @@ async def setup() -> None:
     bot = Ezar()
     log = getLogger(__name__)
     basicConfig(
-        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        format="[%(asctime)s] | %(name)s | %(levelname)s | %(message)s",
         level=INFO,
         datefmt="%Y-%m-%d - %H:%M:%S",
     )
