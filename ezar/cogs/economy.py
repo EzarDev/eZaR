490797e76c2c3331d32ce7cab99af79ca9bbc474
Sham <thesham@proton.me>
2022-08-09 21:31:12 +0200

    feat(economy): add profile view command

--- ezar/cogs/economy.py
+++ ezar/cogs/economy.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+from typing import Optional
+
 from disnake import (
     ApplicationCommandInteraction,
     InteractionMessage,
     Member,
     MessageInteraction,
+    User,
 )
 from disnake.ext.commands import Cog, slash_command
 from disnake.ui import Button, View, button
@@ -10,6 +14,7 @@ from disnake.ui import Button, View, button
 from ezar import Ezar
 from ezar.backend.config import Database, Emojis
 from ezar.helpers.components import disable_components
+from ezar.utils.embed import Embeb
 
 
 class ProfileCreate(View):
@@ -25,14 +30,21 @@ class ProfileCreate(View):
         )
 
     @button(label="Yes, I agree", emoji=Emojis.tick)
-    async def create_agree(self, button: MessageInteraction, itr: Button) -> None:
+    async def create_agree(self, button: Button, itr: MessageInteraction) -> None:
         if not itr.author.id == self.author.id:
             return await itr.response.send_message(
                 f"{Emojis.cross} You are not the author of this command.",
                 ephemeral=True,
             )
 
-        await self.db.insert_one({"_id": itr.user.id, "cash": 0, "bank_cash": 0})
+        await self.db.insert_one(
+            {
+                "_id": itr.user.id,
+                "created_at": datetime.now().timestamp(),
+                "cash": 0,
+                "bank_cash": 0,
+            }
+        )
         await disable_components(
             self,
             itr=itr,
@@ -81,6 +93,51 @@ class Economy(Cog):
             )  # TODO: make a ToS for this
             view.message = await itr.original_message()
             return
+
+    @eco_profile.sub_command("show")
+    async def eco_profile_show(
+        self, itr: ApplicationCommandInteraction, user: Optional[User] = None
+    ):
+        """View a person's profile.
+
+        Parameters
+        ----------
+        user: The user to view."""
+        if not user:
+            user: Member = itr.user
+
+        check = await self.db.find_one({"_id": user.id})
+        if not check:
+            error_msg = " not have a profile"
+            await itr.response.defer()
+            return await itr.edit_original_message(
+                "You do" + error_msg
+                if not check and user == itr.user
+                else f"{user} does" + error_msg
+            )
+
+        name = user.name if not user.nick else f"{user.name} ({str(user.nick)!r})"
+        total = check["cash"] + check["bank_cash"]
+        wealth = (
+            "Broke as hell"
+            if total < 300
+            else "Has a decent amount of money"
+            if total < 1000
+            else "Rich AF"
+        )
+
+        prof_embed = Embeb(
+            color=user.colour,
+            title=name,
+            description=wealth,
+            timestamp=datetime.fromtimestamp(check["created_at"]),
+        )
+        prof_embed.set_thumbnail(user.display_avatar.url)
+        prof_embed.add_field(name="Total Cash Amount", value=total)
+        prof_embed.add_field(name="Cash", value=check["cash"])
+        prof_embed.add_field(name="Credit", value=check["bank_cash"])
+        prof_embed.set_footer(text="Account created at")
+        return await itr.response.send_message(embed=prof_embed)
 
 
 def setup(bot: Ezar):
